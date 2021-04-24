af64d16b8cabe6567796a94f9de489565b044ca9
Shoeb Joarder <shoebjoarder@outlook.com>
2021-04-25 00:03:51 +0200

    Update Counter.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,94 +1,103 @@
-import { Button, Grid, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import { grey } from "@material-ui/core/colors";
+import {Button, Grid, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import {grey} from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
-	containerStyle: {
-		marginTop: theme.spacing(18.75),
-	},
-	counterStyle: {
-		margin: "auto",
-		padding: theme.spacing(6.25),
-		backgroundColor: "#FFC53A",
-		width: theme.spacing(51),
-		height: theme.spacing(50),
-		borderRadius: theme.spacing(1.25),
-	},
-	textStyle: {
-		color: "#444",
-		marginBottom: theme.spacing(6),
-	},
-	gutter: {
-		margin: theme.spacing(0, 0.5, 0, 0.5),
-	},
-	increaseButton: {
-		color: theme.palette.common.white,
-		backgroundColor: theme.palette.success.main,
-		"&:hover": {
-			backgroundColor: theme.palette.success.dark,
-		},
-	},
-	resetButton: {
-		color: theme.palette.common.white,
-		backgroundColor: grey[600],
-		"&:hover": {
-			backgroundColor: grey[700],
-		},
-	},
+  containerStyle: {
+    marginTop: theme.spacing(18.75),
+  },
+  counterStyle: {
+    margin: "auto",
+    padding: theme.spacing(6.25),
+    backgroundColor: "#FFC53A",
+    width: theme.spacing(51),
+    height: theme.spacing(50),
+    borderRadius: theme.spacing(1.25),
+  },
+  textStyle: {
+    color: "#444",
+    marginBottom: theme.spacing(6),
+  },
+  gutter: {
+    margin: theme.spacing(0, 0.5, 0, 0.5),
+  },
+  increaseButton: {
+    color: theme.palette.common.white,
+    backgroundColor: theme.palette.success.main,
+    "&:hover": {
+      backgroundColor: theme.palette.success.dark,
+    },
+  },
+  resetButton: {
+    color: theme.palette.common.white,
+    backgroundColor: grey[600],
+    "&:hover": {
+      backgroundColor: grey[700],
+    },
+  },
 }));
 
 export default function Counter(props) {
-	const { name, count, increase, decrease, reset } = props;
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<>
-			<Grid container className={classes.containerStyle}>
-				<Grid
-					container
-					direction="column"
-					alignItems="center"
-					className={classes.counterStyle}
-				>
-					<Grid item>
-						<Typography variant="h3" className={classes.textStyle}>
-							{name}
-						</Typography>
-					</Grid>
+  return (
+    <>
+      <Grid container className={classes.containerStyle}>
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          className={classes.counterStyle}
+        >
+          <Grid item>
+            <Typography
+              variant="h3"
+              className={classes.textStyle}
+            >
+              {props.name}
+            </Typography>
+          </Grid>
 
-					<Grid item>
-						<Typography variant="h1" className={classes.textStyle}>
-							{count}
-						</Typography>
-					</Grid>
+          <Grid item>
+            <Typography
+              variant="h1"
+              className={classes.textStyle}
+            >
+              {props.count}
+            </Typography>
+          </Grid>
 
-					<Grid container justify="center">
-						<Grid item>
-							<Button
-								variant="contained"
-								onClick={increase}
-								className={classes.increaseButton}
-							>
-								Increase
-							</Button>
-						</Grid>
-						<Grid item className={classes.gutter}>
-							<Button variant="contained" color="secondary" onClick={decrease}>
-								Decrease
-							</Button>
-						</Grid>
-						<Grid item>
-							<Button
-								variant="contained"
-								onClick={reset}
-								className={classes.resetButton}
-							>
-								Reset
-							</Button>
-						</Grid>
-					</Grid>
-				</Grid>
-			</Grid>
-		</>
-	);
+          <Grid container justify="center">
+            <Grid item>
+              <Button
+                variant="contained"
+                className={classes.increaseButton}
+                onClick={props.increase}
+              >
+                Increase
+              </Button>
+            </Grid>
+            <Grid item className={classes.gutter}>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={props.decrease}
+              >
+                Decrease
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                className={classes.resetButton}
+                onClick={props.reset}
+              >
+                Reset
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
